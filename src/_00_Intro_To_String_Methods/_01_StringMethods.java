a12ff0c7974d7edba416a2e0085f27c5ea129657
Pallav <Pallav@DESKTOP-DVPQU53>
2022-04-17 14:28:10 -0700

    Started Module 3!

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -34,13 +34,21 @@ public class _01_StringMethods {
 
     // Given Strings s1 and s2, return the longer String
     public static String longerString(String s1, String s2) {
-        return null;
+      
+        if(s1.length()>s2.length()) {
+        	return s1;
+        }
+    	return s2;
     }
 
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-        return null;
+        if(s.contains("underscores")) {
+        	s = s.replace(" ", "_");
+        	
+        }
+    	return s;
     }
 
     // Return the name of the person whose LAST name would appear first if they
@@ -53,7 +61,9 @@ public class _01_StringMethods {
 
     // Return the sum of all numerical digits in the String
     public static int numeralSum(String s) {
-        return 0;
+        int g = Integer.parseInt(s);
+        
+    	return g;
     }
 
     // Return the number of times String substring appears in String s
